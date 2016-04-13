47c4977ec6ae81cdac18faadbcda5c3425a75eb5
Troy Leach <troyleach29@gmail.com>
2016-04-12 22:01:14 -0500

    took out the subdomain cause I could not get it to work need more info

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ require 'api_constraints'
 MarketPlaceApi::Application.routes.draw do
 
   devise_for :users
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+  namespace :api, defaults: { format: :json } do
     
     scope module: :v1, 
       constraints: ApiConstraints.new(version: 1, default: true) do
